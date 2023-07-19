2b5894b2459fd9ef7a8674c0db6a63d119c1be7e
subodh <subodhpokrel10@gmail.com>
2023-07-20 01:40:14 +0545

    Configured separate page for each type of data.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view-engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
@@ -21,11 +22,8 @@ app.get("/", function (req, res) {
     response.on("data", function (data) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
-      res.write(
-        "The temperature in " + query + " is " + temp + " Degrees celcius. "
-      );
-      res.write("The weather is " + weatherData.weather[0].description);
-      res.send();
+      const weatherDescription = weatherData.weather[0].description;
+      res.render("index.ejs", { query, temp, weatherDescription });
     });
   });
 });
@@ -43,34 +41,13 @@ app.get("/pollution", function (req, res) {
   https.get(Pollution_url, function (response) {
     response.on("data", function (data) {
       const pollutionData = JSON.parse(data);
-      // console.log(pollutionData);
       const airQualityIndex = pollutionData.data.current.pollution.aqius;
-      // console.log(airQualityIndex);
-      if (airQualityIndex <= 50) {
-        res.write("The air quality is Good. ");
-        res.write("Current AQI=" + airQualityIndex);
-      } else if (airQualityIndex > 50 && airQualityIndex <= 100) {
-        res.write("The air quality is Fair. ");
-        res.write("Current AQI=" + airQualityIndex);
-      } else if (airQualityIndex > 100 && airQualityIndex <= 150) {
-        res.write("The air quality is Moderate. ");
-        res.write("Current AQI=" + airQualityIndex);
-      } else if (airQualityIndex > 150 && airQualityIndex <= 200) {
-        res.write("The air quality is Unhelathy. ");
-        res.write("Current AQI=" + airQualityIndex);
-      } else if (airQualityIndex > 200 && airQualityIndex <= 300) {
-        res.write("The air quality is Very Unhealthy. ");
-        res.write("Current AQI=" + airQualityIndex);
-      } else if (airQualityIndex > 300 && airQualityIndex <= 500) {
-        res.write("The air quality is Hazardous. ");
-        res.write("Current AQI=" + airQualityIndex);
-      }
-      res.send();
+      res.render("pollution.ejs", { airQualityIndex });
     });
   });
 });
 app.get("/traffic", function (req, res) {
-  res.sendFile(__dirname + "/traffic.html");
+  res.render("traffic.ejs");
 });
 
 app.listen(3000, function () {
