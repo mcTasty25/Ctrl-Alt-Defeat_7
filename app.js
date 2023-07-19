c265a2fd809dfee702adcea2adae86126925283d
subodh <subodhpokrel10@gmail.com>
2023-07-20 04:39:26 +0545

    Added search cities functionality.

--- app.js
+++ app.js
@@ -4,38 +4,54 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set("view-engine", "ejs");
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  const query = "London";
-  const apikey = "42f1e37e883f929da0d0e4e0fdbbed9c";
-  const unit = "metric";
+  const apiKey = "28cf63de-3ecf-4168-b31d-63a0c075bce8";
+  const defaultCity = "Kathmandu";
   const Weather_url =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    query +
-    "&appid=" +
-    apikey +
-    "&units=" +
-    unit;
+    "https://api.airvisual.com/v2/city?city=" +
+    defaultCity +
+    "&state=Central%20Region&country=Nepal&key=" +
+    apiKey;
   https.get(Weather_url, function (response) {
     response.on("data", function (data) {
       const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
-      const weatherDescription = weatherData.weather[0].description;
-      res.render("index.ejs", { query, temp, weatherDescription });
+      const temp = weatherData.data.current.weather.tp;
+      const humidity = weatherData.data.current.weather.hu;
+      const city = weatherData.data.city;
+      res.render("index.ejs", { temp, humidity, city });
+    });
+  });
+});
+app.post("/", function (req, res) {
+  const apiKey = "28cf63de-3ecf-4168-b31d-63a0c075bce8";
+  const currentCity = req.body.city;
+  const Weather_url =
+    "https://api.airvisual.com/v2/city?city=" +
+    currentCity +
+    "&state=Central%20Region&country=Nepal&key=" +
+    apiKey;
+
+  https.get(Weather_url, function (response) {
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.data.current.weather.tp;
+      const humidity = weatherData.data.current.weather.hu;
+      const city = weatherData.data.city;
+      res.render("index.ejs", { temp, humidity, city });
     });
   });
 });
 
 app.get("/pollution", function (req, res) {
-  const London = {
-    lat: 51.5072,
-    long: 0.1276,
-  };
+  const defaultCity = "Kathmandu";
   const apiKey = "28cf63de-3ecf-4168-b31d-63a0c075bce8";
   const Pollution_url =
-    "https://api.airvisual.com/v2/city?city=Los%20Angeles&state=California&country=USA&key=" +
+    "https://api.airvisual.com/v2/city?city=" +
+    defaultCity +
+    "&state=Central%20Region&country=Nepal&key=" +
     apiKey;
 
   https.get(Pollution_url, function (response) {
@@ -46,8 +62,58 @@ app.get("/pollution", function (req, res) {
     });
   });
 });
+
+app.post("/pollution", function (req, res) {
+  const currentCity = req.body.city;
+  const apiKey = "28cf63de-3ecf-4168-b31d-63a0c075bce8";
+  const Pollution_url =
+    "https://api.airvisual.com/v2/city?city=" +
+    currentCity +
+    "&state=Central%20Region&country=Nepal&key=" +
+    apiKey;
+
+  https.get(Pollution_url, function (response) {
+    response.on("data", function (data) {
+      const pollutionData = JSON.parse(data);
+      const airQualityIndex = pollutionData.data.current.pollution.aqius;
+      res.render("pollution.ejs", { airQualityIndex });
+    });
+  });
+});
+
 app.get("/traffic", function (req, res) {
-  res.render("traffic.ejs");
+  const defaultCity = "Kathmandu";
+  const apiKey = "28cf63de-3ecf-4168-b31d-63a0c075bce8";
+  const locationUrl =
+    "https://api.airvisual.com/v2/city?city=" +
+    defaultCity +
+    "&state=Central%20Region&country=Nepal&key=" +
+    apiKey;
+  https.get(locationUrl, function (response) {
+    response.on("data", function (data) {
+      const locatonData = JSON.parse(data);
+      const lat = locatonData.data.location.coordinates[1];
+      const long = locatonData.data.location.coordinates[0];
+      res.render("traffic", { lat, long });
+    });
+  });
+});
+app.post("/traffic", function (req, res) {
+  const currentCity = req.body.city;
+  const apiKey = "28cf63de-3ecf-4168-b31d-63a0c075bce8";
+  const locationUrl =
+    "https://api.airvisual.com/v2/city?city=" +
+    currentCity +
+    "&state=Central%20Region&country=Nepal&key=" +
+    apiKey;
+  https.get(locationUrl, function (response) {
+    response.on("data", function (data) {
+      const locatonData = JSON.parse(data);
+      const lat = locatonData.data.location.coordinates[1];
+      const long = locatonData.data.location.coordinates[0];
+      res.render("traffic", { lat, long });
+    });
+  });
 });
 
 app.listen(3000, function () {
