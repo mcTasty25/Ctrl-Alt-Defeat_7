0ae3f5bf51341a0f8d41f8288bbb3fa903bc1717
subodh <subodhpokrel10@gmail.com>
2023-07-20 16:44:33 +0545

    Added real time data to dashboard

--- app.js
+++ app.js
@@ -1,85 +1,183 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
-
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function (req, res) {
+app.get("/home", function (req, res) {
   const apiKey = "28cf63de-3ecf-4168-b31d-63a0c075bce8";
   const defaultCity = "Kathmandu";
+
   const Weather_url =
     "https://api.airvisual.com/v2/city?city=" +
     defaultCity +
     "&state=Central%20Region&country=Nepal&key=" +
     apiKey;
-  https.get(Weather_url, function (response) {
-    response.on("data", function (data) {
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.data.current.weather.tp;
-      const humidity = weatherData.data.current.weather.hu;
-      const city = weatherData.data.city;
-      res.render("index.ejs", { temp, humidity, city });
-    });
-  });
-});
-app.post("/", function (req, res) {
-  const apiKey = "28cf63de-3ecf-4168-b31d-63a0c075bce8";
-  const currentCity = req.body.city;
-  const Weather_url =
-    "https://api.airvisual.com/v2/city?city=" +
-    currentCity +
-    "&state=Central%20Region&country=Nepal&key=" +
-    apiKey;
 
-  https.get(Weather_url, function (response) {
-    response.on("data", function (data) {
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.data.current.weather.tp;
-      const humidity = weatherData.data.current.weather.hu;
-      const city = weatherData.data.city;
-      res.render("index.ejs", { temp, humidity, city });
-    });
-  });
-});
-
-app.get("/pollution", function (req, res) {
-  const defaultCity = "Kathmandu";
-  const apiKey = "28cf63de-3ecf-4168-b31d-63a0c075bce8";
   const Pollution_url =
     "https://api.airvisual.com/v2/city?city=" +
     defaultCity +
     "&state=Central%20Region&country=Nepal&key=" +
     apiKey;
 
-  https.get(Pollution_url, function (response) {
-    response.on("data", function (data) {
-      const pollutionData = JSON.parse(data);
-      const airQualityIndex = pollutionData.data.current.pollution.aqius;
-      res.render("pollution.ejs", { airQualityIndex });
-    });
-  });
-});
-
-app.post("/pollution", function (req, res) {
-  const currentCity = req.body.city;
-  const apiKey = "28cf63de-3ecf-4168-b31d-63a0c075bce8";
-  const Pollution_url =
+  const locationUrl =
     "https://api.airvisual.com/v2/city?city=" +
-    currentCity +
+    defaultCity +
     "&state=Central%20Region&country=Nepal&key=" +
     apiKey;
 
-  https.get(Pollution_url, function (response) {
-    response.on("data", function (data) {
-      const pollutionData = JSON.parse(data);
-      const airQualityIndex = pollutionData.data.current.pollution.aqius;
-      res.render("pollution.ejs", { airQualityIndex });
+  // Variables to store the fetched data
+  let temp, city, airQualityIndex, lat, long;
+
+  // Make the API calls
+  https.get(Weather_url, function (weatherResponse) {
+    let weatherData = "";
+
+    weatherResponse.on("data", function (data) {
+      weatherData += data;
+    });
+
+    weatherResponse.on("end", function () {
+      const parsedWeatherData = JSON.parse(weatherData);
+      temp = parsedWeatherData.data.current.weather.tp;
+      city = parsedWeatherData.data.city;
+      humidity = parsedWeatherData.data.current.weather.hu;
+      windSpeed = parsedWeatherData.data.current.weather.ws;
+
+      // Call the next API after the first API response is received
+      https.get(Pollution_url, function (pollutionResponse) {
+        let pollutionData = "";
+
+        pollutionResponse.on("data", function (data) {
+          pollutionData += data;
+        });
+
+        pollutionResponse.on("end", function () {
+          const parsedPollutionData = JSON.parse(pollutionData);
+          airQualityIndex = parsedPollutionData.data.current.pollution.aqius;
+
+          // Call the third API after the second API response is received
+          https.get(locationUrl, function (locationResponse) {
+            let locationData = "";
+
+            locationResponse.on("data", function (data) {
+              locationData += data;
+            });
+
+            locationResponse.on("end", function () {
+              try {
+                const parsedLocationData = JSON.parse(locationData);
+
+                // Check if the location data and coordinates exist before accessing them
+                if (parsedLocationData?.data?.location?.coordinates) {
+                  lat = parsedLocationData.data.location.coordinates[1];
+                  long = parsedLocationData.data.location.coordinates[0];
+                } else {
+                  // Set default values if coordinates are not available
+                  lat = 27.7;
+                  long = 85.31;
+                }
+
+                // Render the template after all API data is fetched
+                res.render("home", {
+                  temp,
+                  humidity,
+                  windSpeed,
+                  city,
+                  airQualityIndex,
+                  lat,
+                  long,
+                });
+              } catch (error) {
+                // Handle any errors that may occur during parsing or API calls
+                console.error("Error processing API response:", error);
+                // Redirect to an error page or display an error message to the user
+                res.status(500).send("Internal Server Error");
+              }
+            });
+          });
+        });
+      });
     });
   });
 });
+
+// app.get("/", function (req, res) {
+//   const apiKey = "28cf63de-3ecf-4168-b31d-63a0c075bce8";
+//   const defaultCity = "Kathmandu";
+//   const Weather_url =
+//     "https://api.airvisual.com/v2/city?city=" +
+//     defaultCity +
+//     "&state=Central%20Region&country=Nepal&key=" +
+//     apiKey;
+//   https.get(Weather_url, function (response) {
+//     response.on("data", function (data) {
+//       const weatherData = JSON.parse(data);
+//       const temp = weatherData.data.current.weather.tp;
+//       const humidity = weatherData.data.current.weather.hu;
+//       const city = weatherData.data.city;
+//       res.render("index.ejs", { temp, humidity, city });
+//     });
+//   });
+// });
+// app.post("/", function (req, res) {
+//   const apiKey = "28cf63de-3ecf-4168-b31d-63a0c075bce8";
+//   const currentCity = req.body.city;
+//   const Weather_url =
+//     "https://api.airvisual.com/v2/city?city=" +
+//     currentCity +
+//     "&state=Central%20Region&country=Nepal&key=" +
+//     apiKey;
+
+//   https.get(Weather_url, function (response) {
+//     response.on("data", function (data) {
+//       const weatherData = JSON.parse(data);
+//       const temp = weatherData.data.current.weather.tp;
+//       const humidity = weatherData.data.current.weather.hu;
+//       const city = weatherData.data.city;
+//       res.render("index.ejs", { temp, humidity, city });
+//     });
+//   });
+// });
+
+// app.get("/pollution", function (req, res) {
+//   const defaultCity = "Kathmandu";
+//   const apiKey = "28cf63de-3ecf-4168-b31d-63a0c075bce8";
+//   const Pollution_url =
+//     "https://api.airvisual.com/v2/city?city=" +
+//     defaultCity +
+//     "&state=Central%20Region&country=Nepal&key=" +
+//     apiKey;
+
+//   https.get(Pollution_url, function (response) {
+//     response.on("data", function (data) {
+//       const pollutionData = JSON.parse(data);
+//       const airQualityIndex = pollutionData.data.current.pollution.aqius;
+//       res.render("pollution.ejs", { airQualityIndex });
+//     });
+//   });
+// });
+
+// app.post("/pollution", function (req, res) {
+//   const currentCity = req.body.city;
+//   const apiKey = "28cf63de-3ecf-4168-b31d-63a0c075bce8";
+//   const Pollution_url =
+//     "https://api.airvisual.com/v2/city?city=" +
+//     currentCity +
+//     "&state=Central%20Region&country=Nepal&key=" +
+//     apiKey;
+
+//   https.get(Pollution_url, function (response) {
+//     response.on("data", function (data) {
+//       const pollutionData = JSON.parse(data);
+//       const airQualityIndex = pollutionData.data.current.pollution.aqius;
+//       res.render("pollution.ejs", { airQualityIndex });
+//     });
+//   });
+// });
 
 app.get("/traffic", function (req, res) {
   const defaultCity = "Kathmandu";
@@ -109,8 +207,14 @@ app.post("/traffic", function (req, res) {
   https.get(locationUrl, function (response) {
     response.on("data", function (data) {
       const locatonData = JSON.parse(data);
-      const lat = locatonData.data.location.coordinates[1];
-      const long = locatonData.data.location.coordinates[0];
+      const longlat = {
+        lat: locatonData.data.location.coordinates[1],
+        long: locatonData.data.location.coordinates[0],
+      };
+      const lat = longlat.lat;
+      const long = longlat.long;
+      // console.log(long, lat);
+
       res.render("traffic", { lat, long });
     });
   });
